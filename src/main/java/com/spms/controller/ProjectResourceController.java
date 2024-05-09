f05faa8d91c67193fc7b81ea63d00582b6b65029
Cikian <cikian@outlook.com>
2024-05-09 23:08:00 +0800

    2

--- src/main/java/com/spms/controller/ProjectResourceController.java
+++ src/main/java/com/spms/controller/ProjectResourceController.java
@@ -1,9 +1,12 @@
 package com.spms.controller;
 
+import com.spms.dto.Result;
+import com.spms.entity.ProjectResource;
 import com.spms.service.ProjectResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/projectResource")
@@ -12,13 +15,19 @@ public class ProjectResourceController {
     @Autowired
     private ProjectResourceService projectCostService;
 
-//    @PostMapping("/estimateCost")
-//    public Result estimateCost(@RequestBody List<ProjectResource> projectCosts){
-//        return projectCostService.estimateCost(projectCosts);
-//    }
-//
-//    @PostMapping("/actualCost")
-//    public Result actualCost(@RequestBody List<ProjectResource> projectCosts){
-//        return projectCostService.actualCost(projectCosts);
-//    }
+    @GetMapping("getMembersByProId/{proId}")
+    public Result getMembersByProId(@PathVariable("proId") int proId) {
+        projectCostService.getMembersByProId(proId);
+        return null;
+    }
+
+   // @PostMapping("/estimateCost")
+   // public Result estimateCost(@RequestBody List<ProjectResource> projectCosts){
+   //     return projectCostService.estimateCost(projectCosts);
+   // }
+   //
+   // @PostMapping("/actualCost")
+   // public Result actualCost(@RequestBody List<ProjectResource> projectCosts){
+   //     return projectCostService.actualCost(projectCosts);
+   // }
 }
