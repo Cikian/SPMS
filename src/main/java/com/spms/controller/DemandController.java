7717671b20d6741fe607de484d0e6a1f5c5b37df
Cikian <cikian@outlook.com>
2024-05-13 01:38:54 +0800

    updated

--- src/main/java/com/spms/controller/DemandController.java
+++ src/main/java/com/spms/controller/DemandController.java
@@ -7,6 +7,9 @@ import com.spms.service.DemandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/demand")
 public class DemandController {
@@ -15,12 +18,15 @@ public class DemandController {
     private DemandService demandService;
 
     @GetMapping("/list/{proId}")
-    public Result getDemandByProId(@PathVariable("proId") Long proId){
-        return demandService.getAllDemandsByProId(proId);
+    public Result getDemandByProId(@PathVariable("proId") Long proId) {
+        Map<String, List<Demand>> demands = demandService.getAllDemandsByProId(proId);
+        Integer code = demands.isEmpty() ? ErrorCode.GET_FAIL : ErrorCode.GET_SUCCESS;
+        String msg = demands.isEmpty() ? "无数据" : "获取成功";
+        return new Result(code, msg, demands);
     }
 
     @PostMapping
-    public Result addDemand(@RequestBody Demand demand){
+    public Result addDemand(@RequestBody Demand demand) {
         System.out.println("新需求：" + demand);
         Boolean b = demandService.addDemand(demand);
         Integer code = b ? ErrorCode.ADD_SUCCESS : ErrorCode.ADD_FAIL;
--- src/main/java/com/spms/service/impl/DemandServiceImpl.java
+++ src/main/java/com/spms/service/impl/DemandServiceImpl.java
@@ -1,14 +1,13 @@
 package com.spms.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.spms.dto.Result;
 import com.spms.entity.Demand;
 import com.spms.mapper.DemandMapper;
 import com.spms.service.DemandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @Title: DemandServiceImpl
@@ -26,6 +25,12 @@ public class DemandServiceImpl implements DemandService {
     @Override
     public Boolean addDemand(Demand demand) {
         Long proId = demand.getProId();
+        if (demand.getFatherDemandId() == 0) {
+            demand.setLevel(0);
+        } else {
+            Demand fatherDemand = demandMapper.selectById(demand.getFatherDemandId());
+            demand.setLevel(fatherDemand.getLevel() + 1);
+        }
         // 查询该项目一共有多少个需求，编号加1
         Integer demandCount = demandMapper.countByProId(proId);
         demand.setDemandNo(demandCount + 1);
@@ -33,11 +38,15 @@ public class DemandServiceImpl implements DemandService {
     }
 
     @Override
-    public Result getAllDemandsByProId(Long proId) {
+    public Map<String, List<Demand>> getAllDemandsByProId(Long proId) {
         LambdaQueryWrapper<Demand> lqw = new LambdaQueryWrapper<>();
         lqw.eq(Demand::getProId, proId);
-        List<Demand> demands = demandMapper.selectList(lqw);
-        return Result.success(demands);
+        List<Demand> allDemands = demandMapper.selectList(lqw);
+        List<Demand> demandsByLevel = processDemands(allDemands);
+        Map<String, List<Demand>> result = new HashMap<>();
+        result.put("allDemands", allDemands);
+        result.put("demandsByLevel", demandsByLevel);
+        return result;
     }
 
     @Override
@@ -65,5 +74,41 @@ public class DemandServiceImpl implements DemandService {
         return List.of();
     }
 
+    private List<Demand> processDemands(List<Demand> demands) {
+        // 分离出demands中元素的不同level的元素
 
+        List<Demand> level0Demands = new ArrayList<>();
+        List<Demand> level1Demands = new ArrayList<>();
+        List<Demand> level2Demands = new ArrayList<>();
+        List<Demand> level3Demands = new ArrayList<>();
+        for (Demand demand : demands) {
+            if (demand.getLevel() == 0) {
+                level0Demands.add(demand);
+            } else if (demand.getLevel() == 1) {
+                level1Demands.add(demand);
+            } else if (demand.getLevel() == 2) {
+                level2Demands.add(demand);
+            } else if (demand.getLevel() == 3) {
+                level3Demands.add(demand);
+            }
+        }
+        findParent(level2Demands, level3Demands);
+        findParent(level1Demands, level2Demands);
+        findParent(level0Demands, level1Demands);
+        return level0Demands;
+
+    }
+
+    private void findParent(List<Demand> fatherDemands, List<Demand> childDemands) {
+        for (Demand fatherDemand : fatherDemands) {
+            Long fatherId = fatherDemand.getDemandId();
+            List<Demand> children = new ArrayList<>();
+            for (Demand childDemand : childDemands) {
+                if (Objects.equals(childDemand.getFatherDemandId(), fatherId)) {
+                    children.add(childDemand);
+                }
+            }
+            fatherDemand.setChildren(children);
+        }
+    }
 }
