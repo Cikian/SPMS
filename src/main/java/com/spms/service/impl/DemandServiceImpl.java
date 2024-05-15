c00955628bc1118631087129364e4d35ffdb5ef8
Cikian <cikian@outlook.com>
2024-05-16 06:53:16 +0800

    基本完善

--- src/main/java/com/spms/service/impl/DemandServiceImpl.java
+++ src/main/java/com/spms/service/impl/DemandServiceImpl.java
@@ -4,11 +4,15 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.spms.entity.Demand;
+import com.spms.entity.DemandActive;
 import com.spms.mapper.DemandMapper;
+import com.spms.service.DemandActiveService;
 import com.spms.service.DemandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 /**
@@ -23,6 +27,8 @@ import java.util.*;
 public class DemandServiceImpl implements DemandService {
     @Autowired
     private DemandMapper demandMapper;
+    @Autowired
+    private DemandActiveService demandActiveService;
 
     @Override
     public Boolean addDemand(Demand demand) {
@@ -36,7 +42,20 @@ public class DemandServiceImpl implements DemandService {
         // 查询该项目一共有多少个需求，编号加1
         Integer demandCount = demandMapper.countByProId(proId);
         demand.setDemandNo(demandCount + 1);
-        return demandMapper.insert(demand) > 0;
+        int i = demandMapper.insert(demand);
+        String activeContent = "";
+        if (demand.getWorkItemType() == 0){
+            activeContent = "史诗";
+        } else if (demand.getWorkItemType() == 1){
+            activeContent = "特性";
+        } else if (demand.getWorkItemType() == 2){
+            activeContent = "用户故事";
+        } else if (demand.getWorkItemType() == 3){
+            activeContent = "任务";
+        }
+        demandActiveService.addActive("创建", activeContent, demand.getDemandId(), "", "");
+
+        return i > 0;
     }
 
     @Override
@@ -44,9 +63,10 @@ public class DemandServiceImpl implements DemandService {
         LambdaQueryWrapper<Demand> lqw = new LambdaQueryWrapper<>();
         lqw.eq(Demand::getProId, proId);
         List<Demand> allDemands = demandMapper.selectList(lqw);
+
         List<Demand> demandsByLevel = processDemands(allDemands);
         Map<String, List<Demand>> result = new HashMap<>();
-        result.put("allDemands", allDemands);
+        result.put("allDemands", demandMapper.selectList(lqw));
         result.put("demandsByLevel", demandsByLevel);
         return result;
     }
@@ -76,13 +96,18 @@ public class DemandServiceImpl implements DemandService {
         return List.of();
     }
 
+    @Transactional
     @Override
     public Boolean changeStatus(Long demandId, Integer status) {
         LambdaUpdateWrapper<Demand> luw = new LambdaUpdateWrapper<>();
         luw.eq(Demand::getDemandId, demandId);
         luw.set(Demand::getDemandStatus, status);
+        Demand oldDemand = demandMapper.selectById(demandId);
+        int update = demandMapper.update(null, luw);
+        Demand newDemand = demandMapper.selectById(demandId);
+        demandActiveService.addActive("修改", "状态", demandId, oldDemand.getDemandStatus().toString(), newDemand.getDemandStatus().toString());
 
-        return demandMapper.update(null, luw) > 0;
+        return update > 0;
     }
 
     @Override
@@ -90,8 +115,20 @@ public class DemandServiceImpl implements DemandService {
         LambdaUpdateWrapper<Demand> luw = new LambdaUpdateWrapper<>();
         luw.eq(Demand::getDemandId, demandId);
         luw.set(Demand::getHeadId, headerId);
+        Demand oldDemand = demandMapper.selectById(demandId);
+        int update = demandMapper.update(null, luw);
+        Demand newDemand = demandMapper.selectById(demandId);
 
-        return demandMapper.update(null, luw) > 0;
+        String oldHeaderId = "";
+        String newHeaderId = "";
+        if (oldDemand.getHeadId() != null){
+            oldHeaderId = oldDemand.getHeadId().toString();
+        }
+        if (newDemand.getHeadId() != null){
+            newHeaderId = newDemand.getHeadId().toString();
+        }
+        demandActiveService.addActive("修改", "负责人", demandId, oldHeaderId, newHeaderId);
+        return update > 0;
     }
 
     @Override
@@ -99,8 +136,12 @@ public class DemandServiceImpl implements DemandService {
         LambdaUpdateWrapper<Demand> luw = new LambdaUpdateWrapper<>();
         luw.eq(Demand::getDemandId, demandId);
         luw.set(Demand::getPriority, priority);
+        Demand oldDemand = demandMapper.selectById(demandId);
+        int update = demandMapper.update(null, luw);
+        Demand newDemand = demandMapper.selectById(demandId);
+        demandActiveService.addActive("修改", "优先级", demandId, oldDemand.getPriority().toString(), newDemand.getPriority().toString());
 
-        return demandMapper.update(null, luw) > 0;
+        return update > 0;
     }
 
     @Override
@@ -108,8 +149,57 @@ public class DemandServiceImpl implements DemandService {
         LambdaUpdateWrapper<Demand> luw = new LambdaUpdateWrapper<>();
         luw.eq(Demand::getDemandId, demandId);
         luw.set(Demand::getDemandDesc, desc);
+        int update = demandMapper.update(null, luw);
+        demandActiveService.addActive("修改", "描述", demandId, "", "");
 
-        return demandMapper.update(null, luw) > 0;
+        return update > 0;
+    }
+
+    @Override
+    public Boolean changeStartTime(Long demandId, LocalDateTime startTime) {
+        System.out.println("changeStartTime" + startTime);
+        System.out.println("id: " + demandId);
+        LambdaUpdateWrapper<Demand> luw = new LambdaUpdateWrapper<>();
+        luw.eq(Demand::getDemandId, demandId);
+        luw.set(Demand::getStartTime, startTime);
+        Demand oldDemand = demandMapper.selectById(demandId);
+        int update = demandMapper.update(null, luw);
+        Demand newDemand = demandMapper.selectById(demandId);
+
+        String fromTime = "";
+        String toTime = "";
+        if (oldDemand.getStartTime() != null){
+            fromTime = oldDemand.getStartTime().toString();
+        }
+        if (newDemand.getStartTime() != null){
+            toTime = newDemand.getStartTime().toString();
+        }
+        demandActiveService.addActive("修改", "开始时间", demandId, fromTime, toTime);
+
+        return update > 0;
+    }
+
+    @Override
+    public Boolean changeEndTime(Long demandId, LocalDateTime endTime) {
+        System.out.println("更改结束时间id: " + demandId + " 结束时间: " + endTime);
+
+        LambdaUpdateWrapper<Demand> luw = new LambdaUpdateWrapper<>();
+        luw.eq(Demand::getDemandId, demandId);
+        luw.set(Demand::getEndTime, endTime);
+        Demand oldDemand = demandMapper.selectById(demandId);
+        int update = demandMapper.update(null, luw);
+        Demand newDemand = demandMapper.selectById(demandId);
+
+        String fromTime = "";
+        String toTime = "";
+        if (oldDemand.getEndTime() != null){
+            fromTime = oldDemand.getEndTime().toString();
+        }
+        if (newDemand.getEndTime() != null){
+            toTime = newDemand.getEndTime().toString();
+        }
+        demandActiveService.addActive("修改", "结束时间", demandId, fromTime, toTime);
+        return update > 0;
     }
 
     @Override
@@ -117,8 +207,21 @@ public class DemandServiceImpl implements DemandService {
         LambdaUpdateWrapper<Demand> luw = new LambdaUpdateWrapper<>();
         luw.eq(Demand::getDemandId, demandId);
         luw.set(Demand::getType, type);
+        Demand oldDemand = demandMapper.selectById(demandId);
+        int update = demandMapper.update(null, luw);
+        Demand newDemand = demandMapper.selectById(demandId);
 
-        return demandMapper.update(null, luw) > 0;
+        String oldType = "";
+        String newType = "";
+        if (oldDemand.getType() != null){
+            oldType = oldDemand.getType().toString();
+        }
+        if (newDemand.getType() != null){
+            newType = newDemand.getType().toString();
+        }
+        demandActiveService.addActive("修改", "需求类型", demandId, oldType, newType);
+
+        return update > 0;
     }
 
     @Override
@@ -126,8 +229,21 @@ public class DemandServiceImpl implements DemandService {
         LambdaUpdateWrapper<Demand> luw = new LambdaUpdateWrapper<>();
         luw.eq(Demand::getDemandId, demandId);
         luw.set(Demand::getSource, source);
+        Demand oldDemand = demandMapper.selectById(demandId);
+        int update = demandMapper.update(null, luw);
+        Demand newDemand = demandMapper.selectById(demandId);
 
-        return demandMapper.update(null, luw) > 0;
+        String oldDemandSource = "";
+        String newDemandSource = "";
+        if (oldDemand.getSource() != null){
+            oldDemandSource = oldDemand.getSource().toString();
+        }
+        if (newDemand.getSource() != null){
+            newDemandSource = newDemand.getSource().toString();
+        }
+        demandActiveService.addActive("修改", "需求来源", demandId, oldDemandSource, newDemandSource);
+
+        return update > 0;
     }
 
     @Override
